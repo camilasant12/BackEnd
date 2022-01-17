08d821e9d6f6a07875eb731fc5586039882df672
Camila Santos <camilasant12@outlook.com>
2022-01-17 18:41:12 -0500

    Clase12 -V2

--- Clase12/Server.js
+++ Clase12/Server.js
@@ -6,16 +6,25 @@ const io = require('socket.io')(server)
 let messages = [
 ];
 
+let messages2 = [
+];
+
 app.use(express.static('public'));
 
 io.on('connection', function(socket) {
     console.log('Un cliente se ha conectado');
     socket.emit('messages', messages); // emitir todos los mensajes a un cliente nuevo 
+    socket.emit('messages2', messages2); // emitir todos los mensajes a un cliente nuevo 
 
     socket.on('new-message', function(data) {
         messages.push(data); // agregar mensajes a array 
         io.sockets.emit('messages', messages); //emitir a todos los clientes
+    });   
+    socket.on('new-message', function(data2) {
+        messages2.push(data2); // agregar mensajes a array 
+        io.sockets.emit('messages2', messages2); //emitir a todos los clientes
     });    
+     
 });
 
 
--- Clase12/public/main.js
+++ Clase12/public/main.js
@@ -1,20 +1,42 @@
 let socket = io.connect(); 
+let socket2 = io.connect();
+//nombre producto
 socket.on('messages', function(data) { 
   console.log(data);
   render(data);
 });
 
+socket2.on('messages2', function(data2) { 
+  console.log(data2);
+  render2(data2);
+});
+
+
 function render(data) { 
+    
     let html = data.map(function(elem, Index){ 
-      return(`<tr>
+      return(`
+              <tr>
                 <td>${elem.product}</td>
                 <td>${elem.precio}</td>
-                <td>${elem.foto}</td>
+                <td><img src="${elem.foto}"></img></td>
               </tr>`     
             ) 
     }).join(" "); 
     document.getElementById('messages').innerHTML = html; 
 }
+
+function render2(data2) { 
+    
+    let html2 = data2.map(function(elem2, Index){ 
+      return(`<div>
+              <strong style="color:blue;">${elem2.author}</strong>: 
+              <em>${elem2.text}</em> </div>`) 
+        }).join(" "); 
+        document.getElementById('messages2').innerHTML = html2; 
+}
+
+
 
 function addMessage(e) { 
     let mensaje = { 
@@ -31,3 +53,18 @@ function addMessage(e) {
 
     return false;
 }
+
+function addMessage2(e) { 
+  let mensaje2 = { 
+    author: document.getElementById('username').value, 
+    text: document.getElementById('texto').value
+  }; 
+  socket.emit('new-message', mensaje2); // new-message es el nombre del evento (recordatorio)
+
+  document.getElementById('texto').value = ''
+  document.getElementById('texto').focus()
+
+
+    return false;
+}
+
