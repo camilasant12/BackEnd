6b917cb8b576b0d1b3fa6624dac373f2c48c1694
Camila Santos <camilasant12@outlook.com>
2022-01-27 16:17:10 -0500

    EntregaClase16 - V1

--- Clase16/mariaDBLite.js
+++ Clase16/mariaDBLite.js
@@ -1,6 +1,6 @@
 var dbLite = require('knex')({ 
     client: 'sqlite3',
-    connection: {filename: './DB/mydb.sqlite'},
+    connection: {filename: './DB/ecomerce.sqlite'},
     useNullAsDefault: true
   })
   
